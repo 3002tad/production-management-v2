be5d93230ae4287a4a03ab508207c0576dae5797
3002tad <dat451303@gmail.com>
2025-12-22 21:45:15 +0700

    sửa side bar cho đồng bộ

--- application/views/warehouse/vbackend.php
+++ application/views/warehouse/vbackend.php
@@ -152,6 +152,15 @@
                                                         <span class="nav-link-text ms-1">Tồn kho thành phẩm</span>
                                                 </a>
                                         </li>
+                                        <hr class="horizontal light mt-3 mb-2">
+                                        <li class="nav-item">
+                                                <a class="nav-link text-white" href="<?= site_url('login/logout'); ?>">
+                                                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                                                        <i class="material-icons opacity-10">logout</i>
+                                                        </div>
+                                                        <span class="nav-link-text ms-1">Đăng xuất</span>
+                                                </a>
+                                        </li>
                                 </ul>
                         </div>
         </aside>
